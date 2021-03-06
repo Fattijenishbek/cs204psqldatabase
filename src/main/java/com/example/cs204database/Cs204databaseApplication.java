a77e940c4adfc31aa4952f541a67da4717fe5721
Fattijenishbek <fattijenishbek01@gmail.com>
2021-03-07 02:56:20 +0600

    added guide

--- src/main/java/com/example/cs204database/Cs204databaseApplication.java
+++ src/main/java/com/example/cs204database/Cs204databaseApplication.java
@@ -2,7 +2,10 @@ package com.example.cs204database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Cs204databaseApplication {
 
@@ -10,4 +13,8 @@ public class Cs204databaseApplication {
         SpringApplication.run(Cs204databaseApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String index(){
+        return "<a href='/users/all'>All users</a>";
+    }
 }
