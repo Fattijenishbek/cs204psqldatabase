0268aecacd8d112aa68f17eaa9c554ffbb32edcf
Fattijenishbek <fattijenishbek01@gmail.com>
2021-03-09 12:50:20 +0600

    thymeleaf with database

--- src/main/java/com/example/cs204database/Cs204databaseApplication.java
+++ src/main/java/com/example/cs204database/Cs204databaseApplication.java
@@ -1,20 +1,32 @@
 package com.example.cs204database;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class Cs204databaseApplication {
+
+    @Autowired
+    private UsersRepository repo;
 
     public static void main(String[] args) {
         SpringApplication.run(Cs204databaseApplication.class, args);
     }
 
     @GetMapping("/")
-    public String index(){
-        return "<a href='/users/all'>All users</a>";
+    public String index(Model model){
+        model.addAttribute("users", repo.findAll());
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String aboutMe(){
+        return "about";
     }
 }
